78e838ee38322cc5671c25013c05301ba0644a50
dukan <nikoladukic04@gmail.com>
2026-03-23 13:20:22 +0100

    Uvedena logicka kontrola 2

---
+++ src/biblioteka/Bibliotea.java
@@ -0,0 +1,33 @@
+package biblioteka;
+
+import java.util.List;
+
+import Interfejs.BibliotekaInterface;
+
+public class Bibliotea implements BibliotekaInterface {
+
+	@Override
+	public void dodajKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void obrisiKnjigu(Knjiga knjiga) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<Knjiga> vratiSveKnjige() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Knjiga> pronadjiKnjigu(Autor autor, long isbn, String naslov, String izdavac) {
+		
+		return null;
+	}
+
+}
